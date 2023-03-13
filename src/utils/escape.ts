ecd7503eccc7c845ecb9e9de09869555b729bb11
German Viescas <germanviescas@gmail.com>
2023-03-13 16:02:36 -0300

    Check out for `null` in escape function

--- src/utils/escape.ts
+++ src/utils/escape.ts
@@ -1,6 +1,7 @@
 /**
  * Escape single quote character (') in strings that are to be inserted in Postgres.
  */
-export function escapeSingleQuote(str: string): string {
+export function escapeSingleQuote(str: string | null) {
+  if (!str) return str
   return str.replace("'", "''")
 }
